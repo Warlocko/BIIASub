58cb546e2dbf5a55b8e5374ef480705b4551e048
Jackson <erickgj14@hotmail.com>
2020-11-02 07:53:52 -0600

    BIIA Migration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {LandingPageComponent} from './modules/shared-module/landing-page/landing-page.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: LandingPageComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
